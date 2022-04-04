ed91d12c21e5b1373a1a5dd0c02576d5fe919866
RodrigoParodi <parodirodrigo0@gmail.com>
2022-04-04 00:05:50 -0300

    tp en proceso

--- TP_1/src/funcion1.c
+++ TP_1/src/funcion1.c
@@ -21,3 +21,58 @@ float precioTarjetaDebito(float operador1){
 
 	return resultado;
 }
+
+float precioTarjetaCredito(float operador1){
+
+	int porcentaje = 25;
+	float resultado;
+	float interes;
+
+	interes= operador1 * porcentaje/100;
+	resultado= operador1 + interes;
+
+
+	return resultado;
+}
+
+float precioBitCoin(float operador1){
+
+	float resultado;
+	float bitCoin = 4606954.55;
+
+	resultado= operador1 / bitCoin;
+
+
+	return resultado;
+}
+
+float precioUnitario(float operador1, int operador2){
+
+	float resultado;
+
+
+	resultado= operador1 / operador2;
+
+
+	return resultado;
+}
+
+float diferenciaPrecios(float operador1, float operador2){
+
+	float resultado;
+
+
+	if(operador1>operador2)
+	{
+		resultado = operador1 - operador2;
+	}
+	else
+	{
+		resultado = operador2 - operador1;
+	}
+
+
+
+
+	return resultado;
+}
